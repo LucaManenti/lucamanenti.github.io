694632f5abef829c7068a82ff6f415353b6c09bd
LucaManenti <lucamanenti97@gmail.com>
2021-09-14 17:40:34 +0200

    all

---
+++ index.php
@@ -0,0 +1,436 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <title>Recondite - Official Site</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/171602ffe6.js" crossorigin="anonymous"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+
+
+</head>
+  <body>
+
+
+<!-- IMG -->
+<div class="container-fluid">
+  <div id="img-header-it">
+    <div class="cont-icon-arrow bounce">
+      <h2>Discover the artist</h2>
+      <a href="#bio"><i class="fas fa-angle-down" aria-hidden="true"></i></a>
+    </div>
+  </div>
+</div>
+
+<!-- BIOGRAPHY -->
+<div id="bio" class="title-section" >
+    <h1 class="title pb-5 ml1">Biography</h1>
+
+  <div class="row pt-5 pb-5 block-scene1 scena1" id="scena1">
+    <div class="col-12 col-md-5 mb-4">
+      <img src="img/b1.jpg" width="100%" class="demo-image first" data-title="Bare landscapes of Bavaria">
+    </div>
+    <div class="col-1"></div>
+    <div class="col-12 col-md-4 ">
+      <div class="box-text-dx">
+          <h3>Birth</h3>
+          <hr>
+          <p class="element"></p>
+        </div>
+    </div>
+  </div>
+
+  <div class="row pt-5 pb-5 block-scene2 scena2" id="scena2">
+    <div class="col-2"></div>
+    <div class="col-12 col-md-4">
+      <div class="box-text-sx">
+          <h3>Studio sessions</h3>
+          <hr>
+          <p class="element2"></p>
+      </div>
+    </div>
+    <div class="col-1"></div>
+    <div class="col-12 col-md-5 mb-4">
+        <img src="img/b2.jpg" width="100%" class="demo-image first" data-title="The first equipment to produce music">
+      </div>
+  </div>
+
+  <div class="row pt-5 scena3" id="scena3">
+    <div class="col-12 col-md-5 mb-4">
+      <img src="img/b3.jpg" width="100%" class="demo-image first" data-title="DGLT Festival, Hannover" data-caption="The fisrt big festival when I played.">
+    </div>
+    <div class="col-1"></div>
+    <div class="col-12 col-md-4">
+      <div class="box-text-dx">
+          <h3>Raised</h3>
+          <hr>
+          <p class="element3"></p>
+        </div>
+    </div>
+  </div>
+
+</div>
+
+<!-- LABELS -->
+<div class="container-fluid ">
+  <div class="title-section pt-5">
+      <h1 class="title ml2">Labels</h1>
+
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-8 pb-5">
+      </div>
+      </div>
+      <div class="col-2"></div>
+    </div>
+  <div class="row ">
+    <div class="col-2"></div>
+    <div class="col-2 pt-5">
+      <div class="project__card">
+
+    <a href="" class="project__image">
+      <img src="img/l1.jpg" class="img-responsive lab">
+    </a>
+
+    <div class="project__detail">
+
+
+      <h2 class="project__title"><a href="#">Dystopian</a></h2>
+
+    </div>
+  </div>
+  </div>
+    <div class="col-1"></div>
+    <div class="col-2 pt-5">
+      <div class="project__card">
+
+    <a href="" class="project__image">
+      <img src="img/l2.jpg" class="img-responsive lab">
+    </a>
+
+    <div class="project__detail">
+
+
+      <h2 class="project__title"><a href="#">Ghost International</a></h2>
+
+    </div>
+  </div>
+  </div>
+    <div class="col-1"></div>
+    <div class="col-2 pt-5">
+      <div class="project__card">
+
+    <a href="" class="project__image">
+      <img src="img/aft2.jpg" class="img-responsive lab">
+    </a>
+
+    <div class="project__detail">
+
+
+      <h2 class="project__title"><a href="#">Afterlife</a></h2>
+
+    </div>
+  </div>
+  </div>
+    <div class="col-2"></div>
+
+
+  </div>
+</div>
+
+<!-- RESIDENT AT -->
+<div class="container-fluid pt-5">
+  <div class="title-section ">
+      <h1 class="title ml3">Dj resident at</h1>
+
+
+  <div class="row pt-5 pb-5">
+    <div class="col-2"></div>
+    <div class="col-3 pt-5">
+      <div class="project__card hovereffect">
+
+    <a href="" class="project__image res1" id="res1" >
+      <img src="img/r1.jpg" class="img-responsive lab">
+      <div class="overr">
+               <h2>Berghain, Berlin</h2>
+       <p>
+         <a href="https://www.berghain.berlin/en/" target="_blank">
+           <i class="fas fa-angle-right"></i>
+         </a>
+       </p>
+      </div>
+    </a>
+
+  </div>
+  </div>
+    <div class="col-2"></div>
+    <div class="col-3 pt-5">
+      <div class="project__card hovereffect">
+
+    <a href="" class="project__image res1" id="res1">
+      <img src="img/r2.jpg" class="img-responsive lab">
+      <div class="overr">
+               <h2>Trouw, Amstedam</h2>
+       <p>
+         <a href="https://www.facebook.com/trouwamsterdam/" target="_blank">
+           <i class="fas fa-angle-right"></i>
+         </a>
+       </p>
+    </a>
+  </div>
+  </div>
+    <div class="col-2"></div>
+
+
+  </div>
+</div>
+</div>
+</div>
+
+<!-- ALBUMS -->
+<div class="container-fluid">
+  <div class="title-section pb-5">
+      <h1 class="title ml4">Albums</h1>
+
+    <div class="row pt-5 pb-5">
+      <div class="col-2"></div>
+      <div class="col-8">
+      </div>
+      <div class="col-2"></div>
+    </div>
+</div>
+<div class="row pt-5 pb-5">
+  <div class="col-2"></div>
+  <div class="col-8">
+  </div>
+  <div class="col-2"></div>
+</div>
+      <div class="row alb1 pt-5" id="alb1">
+        <div class="col-3"></div>
+        <div class="col-6 mouseOver_text">
+
+        <h1><a class="screenshot" href="https://open.spotify.com/album/1ZuK1tX8h9343zZo0ofSue" rel="img/a6.jpg" target="_blank">Hinterland</a></h1><br />
+        <h1><a class="screenshot" href="https://open.spotify.com/album/7JoFdHAOCBzjT6lzWOfcGR" rel="img/a4.jpg" target="_blank">Iffy</a></h1><br />
+        <h1><a class="screenshot" href="https://open.spotify.com/album/42IBvSolW7CAbToElxxLnp" rel="img/a1.jpg" target="_blank">Placid</a></h1><br />
+        <h1><a class="screenshot" href="https://open.spotify.com/album/7b83nACspnmYzaIB2jRc0k" rel="img/a5.jpg" target="_blank">Daemmerlicht</a></h1><br />
+        <h1><a class="screenshot" href="https://open.spotify.com/album/4x5tNSxeHweKYlOBXTY51r" rel="img/a7.jpg" target="_blank">Dwell</a></h1><br />
+        <h1><a class="screenshot" href="" rel="img/a9.jpg">Coming soon</a></h1>
+
+        </div>
+        <div class="col-3"></div>
+        </div>
+</div>
+
+<!-- EVENTS -->
+<div class="container-fluid  pb-5">
+      <div id="section-events">
+        <div class="title-section pb-5" >
+          <h1 class=" ml5">Latest events</h1>
+        </div>
+
+      <div class="row pb-5 pt-5" id="reveal-elements">
+        <div class="col-1"></div>
+        <div class="col-2 pb-5">
+        <div class="card-deck">
+          <div class="card bl bn eve1 eff1">
+            <img src="img/e7.jpg" class="card-img-top" alt="album1" >
+            <div class="card-img-overlay ct1">
+              <p class="card-text white">21/06/2020</p>
+              <h3 class="card-title white">HI, Ibiza</h3>
+            </div>
+          </div>
+          </div>
+        </div>
+          <div class="col-2 pb-5">
+          <div class="card-deck">
+            <div class="card bl bn eve1 eff1">
+              <img src="img/a2.png" class="card-img" alt="album1">
+              <div class="card-img-overlay ct1">
+                <p class="card-text white">15/06/2020</p>
+                <h3 class="card-title white">DRUMCODE FESTIVAL, Amsterdam</h3>
+
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-2 pb-5">
+          <div class="card-deck">
+            <div class="card bl bn eve1 eff1">
+              <img src="img/e1.jpg" class="card-img-top" alt="album1">
+              <div class="card-img-overlay ct1">
+                <p class="card-text white">12/06/2020</p>
+                <h3 class="card-title white">PRIVILEGE, Ibiza</h3>
+
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-2 pb-5">
+          <div class="card-deck">
+            <div class="card bl bg-dark eve1 eff1">
+              <img src="img/e3.jpg" class="card-img-top" alt="album1">
+              <div class="card-img-overlay ct1">
+                <p class="card-text white">12/04/2020</p>
+                <h3 class="card-title white">CERCLE DJ SET, Nice</h3>
+
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-2 pb-5">
+          <div class="card-deck">
+            <div class="card bl bn eve1 eff1">
+              <img src="img/e6.jpg" class="card-img-top" alt="album1">
+              <div class="card-img-overlay ct1">
+                <p class="card-text white">01/01/2020</p>
+                <h3 class="card-title white">COCORICO, Riccione</h3>
+
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-1"></div>
+        </div>
+      </div>
+
+</div>
+
+ <!-- FOLLOW -->
+<div class="container-fluid pt-5 ">
+  <div class="title-section pt-5">
+      <h1 class="title ml6">Follow on</h1>
+
+  <div class="row pt-5">
+    <div class="col-2"></div>
+    <div class="col-2 pt-5">
+      <div class="project__card hovereffect">
+
+    <a href="" class="project__image res1" id="res1" >
+      <img src="img/e4.jpg" class="img-responsive lab">
+      <div class="overr">
+               <h2>Facebook</h2>
+       <p>
+         <a href="https://www.facebook.com/ReconditeMusic/" target="_blank">
+           <i class="fab fa-facebook-f"></i>
+         </a>
+       </p>
+      </div>
+    </a>
+
+  </div>
+  </div>
+    <div class="col-1"></div>
+    <div class="col-2 pt-5">
+      <div class="project__card hovereffect">
+
+    <a href="" class="project__image res1" id="res1">
+      <img src="img/e2.jpg" class="img-responsive lab">
+      <div class="overr">
+               <h2>Twitter</h2>
+       <p>
+         <a href="https://twitter.com/_Recondite_" target="_blank">
+           <i class="fab fa-twitter"></i>
+         </a>
+       </p>
+    </a>
+  </div>
+  </div>
+</div>
+    <div class="col-1"></div>
+    <div class="col-2 pt-5">
+      <div class="project__card hovereffect">
+
+    <a href="" class="project__image res1" id="res1">
+      <img src="img/e3.jpg" class="img-responsive lab">
+      <div class="overr">
+               <h2>Instagram</h2>
+       <p>
+         <a href="https://www.instagram.com/_recondite_/" target="_blank">
+           <i class="fab fa-instagram"></i>
+         </a>
+       </p>
+    </a>
+    </div>
+    </div>
+    </div>
+    <div class="col-2"></div>
+
+  </div>
+</div>
+</div>
+
+<!-- NEWSLETTER -->
+<div class="container-fluid black padd80">
+  <div class="row md-12 padd40">
+      <div class="col-3">
+      </div>
+      <div class="col-6 p">
+        <h1 class="white nl">Keep in touch</h1>
+        <br />
+        <form>
+  <div class="form-group">
+    <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your e-mail address">
+  </div>
+  <a href="javascript:;" class="bx padd40">
+    <svg class="icon-arrow before">
+        <use xlink:href="#arrow"></use>
+    </svg>
+    <span class="label">Subscribe</span>
+    <svg class="icon-arrow after">
+        <use xlink:href="#arrow"></use>
+    </svg>
+</a>
+
+<svg style="display: none;">
+  <defs>
+    <symbol id="arrow" viewBox="0 0 35 15">
+
+      <path d="M27.172 5L25 2.828 27.828 0 34.9 7.071l-7.07 7.071L25 11.314 27.314 9H0V5h27.172z "/>
+    </symbol>
+  </defs>
+</svg>
+  </form>
+      </div>
+      <div class="col-3">
+      </div>
+    </div>
+   </div>
+</div>
+    </div>
+
+<!-- FOOTER -->
+<div class="container-fluid d-none d-md-block d-xl-block">
+  <div class="row md-12 padd40">
+    <div class="col-3">
+    </div>
+    <div class="col-6 p">
+
+      <h6>Recondite™ 2020 | Design by Luca Manenti</h6>
+    </div>
+    <div class="col-3">
+    </div>
+  </div>
+</div>
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+<!-- SCROLL MAGIC -->
+<script type="text/javascript" src="js/main.js"></script>
+<!-- JQUERY E BOOTSTRAP -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-2.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js"></script>
+</body>
+</html>
